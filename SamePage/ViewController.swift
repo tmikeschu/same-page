320bd8c8d35d38ad8c3b7f1f3821b9fcb7e269e0
Mike Schutte <tmikeschutte@gmail.com>
2017-02-18 16:41:55 -0700

    Initial Commit

---
+++ SamePage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SamePage
+//
+//  Created by Mike Schutte on 2/18/17.
+//  Copyright © 2017 T. Mike Schutte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
